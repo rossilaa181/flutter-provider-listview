717dbab8334d5ff9f858b1233a02d29f4300992a
rossilaa181 <latansasalsabela@gmail.com>
2022-10-31 11:38:36 +0700

    Add Dynamic ListView

--- lib/page/addtask.dart
+++ lib/page/addtask.dart
@@ -3,7 +3,9 @@ import 'package:provider/provider.dart';
 import 'package:provider_listview/service/tasklist.dart';
 
 class AddTaskPage extends StatelessWidget {
-  const AddTaskPage({super.key});
+  AddTaskPage({super.key});
+  final TextEditingController _controller = TextEditingController();
+  String task = ''; // This variable holds the tasks user wants to add
 
   @override
   Widget build(BuildContext context) {
@@ -19,6 +21,11 @@ class AddTaskPage extends StatelessWidget {
               decoration: const InputDecoration(
                 hintText: "Masukkan Task Baru",
               ),
+              onChanged: (value) {
+                // This saves the value in the TextField for every character the user types
+                task =
+                    value; // The value in the TextField is referred to by the 'value' variable
+              },
             ),
             const SizedBox(
               height: 50,
@@ -28,7 +35,8 @@ class AddTaskPage extends StatelessWidget {
                 Expanded(
                   child: ElevatedButton(
                     onPressed: () {
-                      context.read<Tasklist>().addNewTask();
+                      context.read<Tasklist>().addNewTask(
+                          task); //This is where I am calling the function to add a task to the list.
                       Navigator.pop(context);
                     },
                     child: const Text("Tambah Task Baru"),
--- lib/service/tasklist.dart
+++ lib/service/tasklist.dart
@@ -6,10 +6,14 @@ class Tasklist with ChangeNotifier {
 
   get taskList => _taskList;
 
-  void addNewTask() {
+  void addNewTask(String taskName) {
     _taskList.add(
-      Task(name: "Task Baru", status: false),
+      Task(name: taskName, status: false),
     );
     notifyListeners();
+    // This is why we use providers. This function notifies all the children widgets
+    // of the Widget where we initiated our provider (see the parent of MaterialApp Widget in the above code)
+    // This is why changes to data should be made within this class only as it extends ChangeNotifier,
+    // which provides us with notifyListeners() method. Which ultimately notifies the widgets that the data has been modified and its time to rebuild the widgets that rely on this data!
   }
 }
